52afdbd470689b3c4a27a4b09c6e0982d24f4ccc
n3xSU5 <yormanlopez786@gmail.com>
2026-05-02 00:40:56 -0500

    Actualización de las rutas

--- src/modules/orders/routers/ordersRouters.jsx
+++ src/modules/orders/routers/ordersRouters.jsx
@@ -1,8 +1,10 @@
 import { Route } from "react-router-dom";
 import OrdersPage from "../pages/OrdersPage";
+import NewOrderPage from "../pages/NewOrderPage";
 
 export const ordersRoutes = (
     <>
         <Route path="/pedidos" element={<OrdersPage />} />
+        <Route path="/nuevo-pedido" element={<NewOrderPage />} />
     </>
 );
