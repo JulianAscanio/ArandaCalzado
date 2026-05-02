6a5940843dbc74b4fd7521736acd5f827447ed80
n3xSU5 <yormanlopez786@gmail.com>
2026-05-01 19:54:12 -0500

    Arreglo menor

--- src/modules/orders/pages/OrdersPage.jsx
+++ src/modules/orders/pages/OrdersPage.jsx
@@ -73,7 +73,7 @@ export default function OrdersPage() {
       {
         selectedOrder && (
           <StageModal
-            material={selectedOrder}
+            order={selectedOrder}
             onClose={() => setSelectedOrder(null)}
           />
         )
